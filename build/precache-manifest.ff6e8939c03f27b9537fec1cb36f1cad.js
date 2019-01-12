95c7701aa9165c376198a2b3e8c749bd8cc7fd54
jgreg728 <jjg070890@aol.com>
2019-01-12 14:13:52 -0500

    saving deployment

---
+++ build/precache-manifest.ff6e8939c03f27b9537fec1cb36f1cad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a780af1f80bc7ebb940",
+    "url": "/Click-Game-2/static/js/runtime~main.2a780af1.js"
+  },
+  {
+    "revision": "f2d13e77dfa5fd6b2692",
+    "url": "/Click-Game-2/static/js/main.f2d13e77.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/Click-Game-2/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "f2d13e77dfa5fd6b2692",
+    "url": "/Click-Game-2/static/css/main.d4ef25f7.chunk.css"
+  },
+  {
+    "revision": "048874142fef007eb842ecf6568f3c64",
+    "url": "/Click-Game-2/index.html"
+  }
+];
